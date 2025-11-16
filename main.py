c90109ea9617d7e398d8ddd2b43d2ace3e4f156f
Prakhar <prakharrathore111@gmail.com>
2025-11-16 18:44:36 +0530

    made changes for delete req

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 from dotenv import load_dotenv
 import certifi
+from bson import ObjectId
+
 
 load_dotenv()
 
@@ -33,15 +35,6 @@ class EuronData(BaseModel):
     course: str
 
 
-@app.post("/euron/insert")
-async def insert_euron_data(data: EuronData):
-    result = await euron_data.insert_one(data.dict())
-    return {
-        "message": "Data inserted successfully",
-        "id": str(result.inserted_id)
-    }
-
-
 @app.get("/euron/getdata")
 async def get_euron_data():
     items = []
@@ -50,3 +43,28 @@ async def get_euron_data():
         document["_id"] = str(document["_id"])
         items.append(document)
     return items
+
+
+@app.post("/euron/insert")
+async def insert_euron_data(data: EuronData):
+    result = await euron_data.insert_one(data.dict())
+    inserted_doc = await euron_data.find_one({"_id": result.inserted_id})
+    inserted_doc["_id"] = str(inserted_doc["_id"])
+    inserted_doc["message"] = "Data inserted successfully"
+    return inserted_doc
+
+
+@app.delete("/euron/delete/{item_id}")
+async def delete_euron_data(item_id: str):
+    if not ObjectId.is_valid(item_id):
+        raise HTTPException(status_code=400, detail="Invalid ObjectId format")
+
+    doc = await euron_data.find_one({"_id": ObjectId(item_id)})
+
+    if not doc:
+        raise HTTPException(status_code=404, detail="Document not found")
+
+    await euron_data.delete_one({"_id": ObjectId(item_id)})
+    doc["_id"] = str(doc["_id"])
+    doc["message"] = "Document deleted successfully"
+    return doc
